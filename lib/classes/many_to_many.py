4fadac661d4f4aa0b6e4ca79139a0581bcc73eee
Emmanuel Wambugu <emmanuelwambugu5@gmail.com>
2025-05-25 16:30:27 +0300

    Added getters and setters to the magazine and author classes as well as validator methods.

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -3,10 +3,22 @@ class Article:
         self.author = author
         self.magazine = magazine
         self.title = title
-        
+
+
 class Author:
     def __init__(self, name):
-        self.name = name
+        self._validate_name(name)
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    def _validate_name(self, name):
+        if hasattr(self, '_name'):
+            raise AttributeError("Cannot modify name after initialization")
+        if len(name.strip()) == 0 or not isinstance(self.name, str):
+            raise ValueError("Author name must be a non-empty string.")
 
     def articles(self):
         pass
@@ -20,10 +32,25 @@ class Author:
     def topic_areas(self):
         pass
 
+
 class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._validate_name(value)
+        self._name = value
+
+    def _validate_name(self, name):
+        if 2 < len(name.strip()) > 16 or not isinstance(name, str):
+            raise ValueError(
+                "Magazine name must be a string between 2 and 16 characters.")
 
     def articles(self):
         pass
